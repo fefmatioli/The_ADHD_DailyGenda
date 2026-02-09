b7e6cf7fa18ed3df36caf6862b6c80e3d4bce99d
Fernanda F. Matioli <153054831+fefmatioli@users.noreply.github.com>
2026-02-08 23:29:14 -0300

    Mudança de Layout- pt 1
    Redesign simples de todo o site e suas telas

--- api_project/core/urls.py
+++ api_project/core/urls.py
@@ -3,7 +3,7 @@ from django.contrib.auth import views as auth_views
 from . import views_site
 
 urlpatterns = [
-    path("", views_site.dashboard, name="dashboard"),
+    path("", views_site.dashboard, name="dashboard"), #
     path("create-note/", views_site.create_note, name="create_note"),
     path("create-task/", views_site.create_task, name="create_task"),
     path("create-event/", views_site.create_event, name="create_event"),
@@ -15,7 +15,7 @@ urlpatterns = [
     path("event/<int:event_id>/", views_site.event_detail, name="event_detail"),
     path("event/<int:event_id>/edit/", views_site.event_edit, name="event_edit"),
     path("note/<int:note_id>/edit/", views_site.note_edit, name="note_edit"),
-    path("login/", auth_views.LoginView.as_view(template_name="login.html"), name="login"),
-    path("logout/", auth_views.LogoutView.as_view(), name="logout"),
-    path("signup/", views_site.signup, name="signup"),
+    path("login/", auth_views.LoginView.as_view(template_name="login.html"), name="login"), 
+    path("logout/", auth_views.LogoutView.as_view(), name="logout"), 
+    path("signup/", views_site.signup, name="signup"), 
 ]
